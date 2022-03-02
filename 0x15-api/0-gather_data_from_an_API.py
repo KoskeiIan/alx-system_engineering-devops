b861f667a4193d3a1abb50a6734e0a923cf8cc0a
KoskeiIan <ian.k.koskei@gmail.com>
2022-03-02 14:44:11 +0300

    readme

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
-
-""" getting data from an API"""
-
-import sys
+"""
+Using a REST API, for a given employee ID and returns information
+about his/her TODO list progress.
+"""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    id = sys.argv[1]
-    tasks = []
-    complete = 0
-    total_tasks = 0
-    url = "https://jsonplaceholder.typicode.com/users/" + id
-    res = requests.get(url).json()
-    name = res.get('name')
-    url = "https://jsonplaceholder.typicode.com/todos"
-    res_tasks = requests.get(url).json()
 
-    for i in res_tasks:
-        if i.get("userId") == int(id):
-            if i.get("completed") is True:
-                tasks.append(i["title"])
-                complete += 1
-        total_tasks += 1
-    print("Employee {} is done with tasks ({}/{}):"
-          .format(name, complete, total_tasks))
+    id_user = int(sys.argv[1])
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
 
-    for x in tasks:
-        print("\t {}".format(x))
+    req_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    req_user = requests.get(
+        'https://jsonplaceholder.typicode.com/users').json()
+
+    print('Employee ', end='')
+
+    for i in req_user:
+        if id_user == i.get('id'):
+            EMPLOYEE_NAME = i.get('name')
+
+    for j in req_todos:
+        total_task = j.get('userId')
+        if total_task == id_user:
+            TOTAL_NUMBER_OF_TASKS += 1
+
+            number_task = j.get('completed')
+            if number_task is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(j.get('title'))
+
+    print('{} is done with tasks({}/{}):'.format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for j in TASK_TITLE:
+        print('\t {}'.format(j))
